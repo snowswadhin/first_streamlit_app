b2e0276c267fc8b482223df3b881d90869c1a478
snowswadhin <135094022+snowswadhin@users.noreply.github.com>
2023-05-31 11:44:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New healthy Diner')
+streamlit.title('Breakfast Manu')
+streamlit.title('Omega 3 & blueberry Outmeal')
+streamlit.title('Kale ,spinach & Rocket Smoothie')
+streamlit.title('My Parents New healthy Diner')

4286e62c6909784223046153cc9e256e66343632
snowswadhin <135094022+snowswadhin@users.noreply.github.com>
2023-06-01 11:16:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response=request.get("https://fruityvice.com/api/fruit/watermelon")
+stremlit.text(fruityvice_response.json())
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.jason())
+streamlit.dataframe(fruityvice_normalized)
